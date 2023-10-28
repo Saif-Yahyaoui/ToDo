0c59997994d144ccf8947d1338535fe6a3257a95
Saif Yahyaoui <saif.yahyaoui@esprit.tn>
2023-10-28 18:07:59 +0100

    List ToDo in Home

--- app/src/main/java/tn/esprit/todo/fragments/HomeFragment.kt
+++ app/src/main/java/tn/esprit/todo/fragments/HomeFragment.kt
@@ -8,25 +8,34 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import tn.esprit.todo.databinding.FragmentHomeBinding
+import tn.esprit.todo.utils.ToDoAdapter
+import tn.esprit.todo.utils.ToDoData
 
-class HomeFragment : Fragment(), AddToDoFragment.DialogNextBtnClickListener {
+class HomeFragment : Fragment(), AddToDoFragment.DialogNextBtnClickListener,
+    ToDoAdapter.ToDoAdapterClicksInterface {
     private lateinit var auth: FirebaseAuth
     private lateinit var databaseRef: DatabaseReference
     private lateinit var navController: NavController
     private lateinit var binding: FragmentHomeBinding
     private lateinit var popUpFragment: AddToDoFragment
+    private lateinit var adapter: ToDoAdapter
+    private lateinit var mList: MutableList<ToDoData>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(inflater , container , false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -34,11 +43,13 @@ class HomeFragment : Fragment(), AddToDoFragment.DialogNextBtnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         init(view)
+        getDataFromFirebase()
         registerEvents()
 
 
     }
-    private fun registerEvents(){
+
+    private fun registerEvents() {
         binding.addTaskBtn.setOnClickListener {
             popUpFragment = AddToDoFragment()
             popUpFragment.setListener(this)
@@ -48,23 +59,69 @@ class HomeFragment : Fragment(), AddToDoFragment.DialogNextBtnClickListener {
         }
     }
 
-    private fun init(view: View){
+    private fun init(view: View) {
         navController = Navigation.findNavController(view)
         auth = FirebaseAuth.getInstance()
         databaseRef = FirebaseDatabase.getInstance().reference
             .child("Tasks").child(auth.currentUser?.uid.toString())
+        binding.mainRecyclerView.setHasFixedSize(true)
+        binding.mainRecyclerView.layoutManager = LinearLayoutManager(context)
+        mList = mutableListOf()
+        adapter = ToDoAdapter(mList)
+        adapter.setListener(this)
+        binding.mainRecyclerView.adapter = adapter
+
+
+    }
+
+    private fun getDataFromFirebase() {
+        databaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                mList.clear()
+                for (taskSnapshot in snapshot.children) {
+                    val todoTask = taskSnapshot.key?.let {
+                        ToDoData(it, taskSnapshot.value.toString())
+                    }
+                    if (todoTask != null) {
+                        mList.add(todoTask)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
     }
 
     override fun onSaveTask(todo: String, todoEt: TextInputEditText) {
         databaseRef.push().setValue(todo).addOnCompleteListener {
-            if(it.isSuccessful){
-                Toast.makeText(context,"Todo Saved Successfully" , Toast.LENGTH_SHORT).show()
+            if (it.isSuccessful) {
+                Toast.makeText(context, "Todo Saved Successfully", Toast.LENGTH_SHORT).show()
                 todoEt.text = null
-            }else{
-                Toast.makeText(context,it.exception?.message ,Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
             }
             popUpFragment.dismiss()
         }
+    }
+
+    override fun onDeleteTaskBtnClicked(toDoData: ToDoData) {
+        databaseRef.child(toDoData.taskId).removeValue().addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(context, "Todo Deleted Successfully", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onEditTaskBtnClicked(toDoData: ToDoData) {
+        TODO("Not yet implemented")
     }
 
 
---
+++ app/src/main/java/tn/esprit/todo/utils/ToDoData.kt
@@ -0,0 +1,3 @@
+package tn.esprit.todo.utils
+
+data class ToDoData(val taskId:String , val task : String)
